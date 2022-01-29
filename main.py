3fe670c7ee759ec4684e6fc3cb5490e854d75301
Gusmao <williamgusmao@hotmail.com>
2022-01-29 19:04:39 -0300

    Feito

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import random
+from tokenize import String
+
+
+def Sort():
+    pasta = input("Cole o diretorio aqui: ")
+    arq = ""
+    for diretorio, subpastas, arquivos in os.walk(pasta):
+        for arquivo in arquivos:
+            #print(os.path.join(os.path.realpath(diretorio), arquivo))
+            arq = os.path.join(os.path.realpath(diretorio), arquivo)
+            teste = arq[len(pasta)+1:]
+            random.seed(None)
+            os.rename(arq, f'{pasta}/{str(random.randrange(0, 9999))}_{teste}')
+    print("Finshed!")
+
+def Remove():
+    pasta = input("Cole o diretorio aqui: ")
+    arq = ""
+    for diretorio, subpastas, arquivos in os.walk(pasta):
+        for arquivo in arquivos:
+            #print(os.path.join(os.path.realpath(diretorio), arquivo))
+            arq = os.path.join(os.path.realpath(diretorio), arquivo)
+            teste = arq[len(pasta)+6:]
+            random.seed(None)
+            os.rename(arq, f'{pasta}/{teste}')
+    print("Finshed!")
+
+x = input("Digite (1) para sortear e (2) para remover numeros: ")
+
+if x == "1":
+    Sort()
+if x == "2":
+    Remove()
